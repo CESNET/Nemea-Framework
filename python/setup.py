2574a2916edc5ff4cdf403a96ccfc6dfec64e6d1
Tomas Cejka <cejkat@cesnet.cz>
2016-02-10 12:50:34 +0100

    python: RPM: rewrite process of package build

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,18 @@
 from distutils.core import setup
 
-setup(name='trap+unirec',
+DESCRIPTION="Python wrapper for NEMEA Framework"
+LONG_DESCRIPTION="""This distribution contains two related modules/packages:
+  trap: Python wrapper for libtrap - an implementation of Traffic Analysis Platform (TRAP)
+  unirec: Python version of a data structure used in TRAP
+"""
+setup(name='nemea-python',
       version='1.0',
       py_modules=['trap'],
       packages=['unirec'],
       author='Vaclav Bartos, CESNET',
       author_email='bartos@cesnet.cz',
       license="BSD",
-      description="""This distribution contains two related modules/packages:
-  trap: Python wrapper for libtrap - an implementation of Traffic Analysis Platform (TRAP)
-  unirec: Python version of a data structure used in TRAP
-""",
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION
       )
 
