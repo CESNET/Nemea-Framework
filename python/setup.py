a7e8ebf5348ac2aa10fb2adcc59d26e6b7338bb3
Tomas Jansky <janskto1@fit.cvut.cz>
2016-02-11 14:16:29 +0100

    python: update version of package

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ LONG_DESCRIPTION="""This distribution contains two related modules/packages:
   unirec: Python version of a data structure used in TRAP
 """
 setup(name='nemea-python',
-      version='1.0',
+      version='2.0.0',
       py_modules=['trap'],
       packages=['unirec'],
       author='Vaclav Bartos, CESNET',
