0e1677750cf6bfdbdf0377ab5016234dc9b4d3db
Tomas Cejka <cejkat@cesnet.cz>
2016-02-11 13:15:30 +0100

    pycommon: add common module/package for python code

---
+++ pycommon/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+DESCRIPTION="Common Python modules and methods of the NEMEA system."
+LONG_DESCRIPTION="""The module contains methods for creation and submission of incident reports in IDEA format."""
+setup(name='nemea-pycommon',
+      version='1.0',
+      py_modules=['report2idea.py'],
+      packages=[],
+      author='Vaclav Bartos, CESNET',
+      author_email='bartos@cesnet.cz',
+      license="BSD",
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION
+      )
+
