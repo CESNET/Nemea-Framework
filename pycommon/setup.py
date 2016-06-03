b8bacad25aa3d270a63f30e7adef5dd6403a71b2
Tomas Jansky <janskto1@fit.cvut.cz>
2016-06-03 23:09:50 +0200

    pycommon: increased version, released RPM package

--- pycommon/setup.py
+++ pycommon/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 DESCRIPTION="Common Python modules and methods of the NEMEA system."
 LONG_DESCRIPTION="""The module contains methods for creation and submission of incident reports in IDEA format."""
 setup(name='nemea-pycommon',
-      version='1.0.4',
+      version='1.0.5',
       py_modules=['report2idea'],
       packages=[],
       author='Vaclav Bartos, CESNET',
