9545cee3c452ebb8171a311b9d1a0d80c1d5f5ca
Tomas Cejka <cejkat@cesnet.cz>
2016-06-28 23:22:25 +0200

    pytrap: experiments with pypi, renamed package to unique name

--- pytrap/setup.py
+++ pytrap/setup.py
@@ -4,11 +4,12 @@ pytrapmodule = Extension('pytrap',
                     sources = ['pytrapmodule.c', 'unirecmodule.c', 'fields.c'],
                     libraries = ['trap', 'unirec'])
 
-setup(name = 'pytrap',
+setup(name = 'nemea-pytrap',
        version = '0.9',
        description = 'The pytrap module is a native Python extension that allows for writing NEMEA modules in Python.',
        author = 'Tomas Cejka',
        author_email = 'cejkat@cesnet.cz',
        url = 'https://github.com/CESNET/Nemea-Framework',
+       license = 'BSD',
        ext_modules = [pytrapmodule])
 
