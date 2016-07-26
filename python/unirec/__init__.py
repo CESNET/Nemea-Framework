40e53e8231863bd50a6727bc0c1be6283909fcc3
Tomas Cejka <cejkat@cesnet.cz>
2016-07-26 10:59:30 +0200

    python: remove, it is obsoleted by pytrap/ which is a beta version now

--- python/unirec/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ["unirec",  "ur_ipaddr",  "ur_time",  "ur_types"]
-from unirec import *
-try:
-    from unirec.unirec import *
-except:
-    # workaround for python2
-    from .unirec import *
-    from unirec import *
-    pass
