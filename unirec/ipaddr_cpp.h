b2b1f976dc17df24e54c49525c752ab17ec31823
Tomas Cejka <cejkat@cesnet.cz>
2016-12-08 19:56:33 +0100

    unirec: add doxygen for ipaddr_cpp.h
    Related to #36.

--- unirec/ipaddr_cpp.h
+++ unirec/ipaddr_cpp.h
@@ -6,11 +6,20 @@
 #include <iostream>
 #include <arpa/inet.h>     // INET6_ADDRSTRLEN value
 
+/**
+ * IPaddr_cpp class representing IP address.
+ *
+ * Wrapper of IP address data type (ip_addr_t) for C++.
+ */
 class IPaddr_cpp {
    bool new_object;
 public:
    const ip_addr_t *data;
    inline IPaddr_cpp();
+
+   /**
+    * Copy constructor of IPaddr_cpp class.
+    */
    inline IPaddr_cpp(const ip_addr_t *ptr) : data(ptr) {new_object = false;};
    inline ~IPaddr_cpp();
    bool operator<(const IPaddr_cpp &key2) const;
@@ -22,18 +31,29 @@ public:
    std::string toString() const;
 };
 
+/**
+ * Constructor of IPaddr_ccp object.
+ */
 inline IPaddr_cpp::IPaddr_cpp()
 {
    data = new ip_addr_t;
    new_object = true;
 }
 
+/**
+ * Desctructor of IPaddr_ccp object.
+ */
 inline IPaddr_cpp::~IPaddr_cpp()
 {
     if (new_object) delete data;
 }
 
-// swap_bytes_
+/**
+ * Swap bytes of 64b number.
+ *
+ * \param[in] x 64b number to swap bytes for.
+ * \returns Number with swapped bytes (least significant byte becomes most significant byte in result)
+ */
 inline uint64_t swap_bytes(const uint64_t x)
 {
    return
@@ -47,35 +67,58 @@ inline uint64_t swap_bytes(const uint64_t x)
       ((x & 0xff00000000000000LL) >> 56);
 }
 
-// Comparison operators
+/**
+ * Compare IP address (operator <)
+ */
 inline bool IPaddr_cpp::operator<(const IPaddr_cpp &key2) const {
    return ((swap_bytes(this->data->ui64[0]) < swap_bytes(key2.data->ui64[0])) ||
            ((swap_bytes(this->data->ui64[0]) == swap_bytes(key2.data->ui64[0])) &&
             (swap_bytes(this->data->ui64[1]) < swap_bytes(key2.data->ui64[1]))));
 }
 
+/**
+ * Compare IP address (operator <=)
+ */
 inline bool IPaddr_cpp::operator<=(const IPaddr_cpp &key2) const {
    return !(*this > key2);
 }
 
+/**
+ * Compare IP address (operator >)
+ */
 inline bool IPaddr_cpp::operator>(const IPaddr_cpp &key2) const {
    return ((swap_bytes(this->data->ui64[0]) > swap_bytes(key2.data->ui64[0])) ||
            ((swap_bytes(this->data->ui64[0]) == swap_bytes(key2.data->ui64[0])) &&
             (swap_bytes(this->data->ui64[1]) > swap_bytes(key2.data->ui64[1]))));
 }
 
+/**
+ * Compare IP address (operator >=)
+ */
 inline bool IPaddr_cpp::operator>=(const IPaddr_cpp &key2) const {
    return !(*this < key2);
 }
 
+/**
+ * Compare IP address (operator ==)
+ */
 inline bool IPaddr_cpp::operator==(const IPaddr_cpp &key2) const {
    return ((this->data->ui64[0] == key2.data->ui64[0]) && (this->data->ui64[1] == key2.data->ui64[1]));
 }
+
+/**
+ * Compare IP address (operator !=)
+ *
+ */
 inline bool IPaddr_cpp::operator!=(const IPaddr_cpp &key2) const {
    return !(*this == key2);
 }
 
-//String conversions
+/**
+ * Convert IP address to string.
+ *
+ * \returns IP address as a string.
+ */
 inline std::string IPaddr_cpp::toString() const
 {
    char buf[INET6_ADDRSTRLEN];
@@ -83,6 +126,13 @@ inline std::string IPaddr_cpp::toString() const
    return std::string(buf);
 }
 
+/**
+ * Print IP address as string.
+ *
+ * \param[in] os  output stream
+ * \param[in] ip  IP address to print
+ * \returns output stream
+ */
 inline std::ostream& operator<<(std::ostream &os, const IPaddr_cpp &ip)
 {
   return os << ip.toString();
