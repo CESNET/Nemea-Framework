2cb29d7f173d2775eb8dfe259b2acbcc80cf30b5
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-29 09:28:28 +0200

    RC: Parser should raise Exception when parsing fails

--- pycommon/reporter_config/Parser.py
+++ pycommon/reporter_config/Parser.py
@@ -1,24 +1,24 @@
 import yaml
 
 class Parser:
-    config = None
+	config = None
 
-    def __init__(self, config):
-        self.config = self.load(config)
+	def __init__(self, config):
+		self.config = self.load(config)
 
-    @classmethod
-    def load(self, config):
-        """Load and parse given string via yaml.load
-        In case of YAML failing to parse return None.
-        """
-        try:
-            return yaml.load(config)
-        except Exception as e:
-            print(e)
-            return None
+	@classmethod
+	def load(self, config):
+		"""Load and parse given string via yaml.load
+		In case of YAML failing to parse return None.
+		"""
+		try:
+			return yaml.load(config)
+		except Exception as e:
+			print(e)
+			raise Exception("Error while parsing config file")
 
-    def __str__(self):
-        return yaml.dump(self.config)
+	def __str__(self):
+		return yaml.dump(self.config)
 
-    def __getitem__(self, key):
-        return self.config[key]
+	def __getitem__(self, key):
+		return self.config[key]
