3cf2bd3223f58cbbf4e9d3b7d974cebc2a760e3a
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-06-01 17:24:45 +0200

    Fix WardenAction

--- pycommon/reporter_config/actions/Warden.py
+++ pycommon/reporter_config/actions/Warden.py
@@ -2,26 +2,24 @@ from .Action import Action
 
 import warden_client
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
 class WardenAction(Action):
-    def __init__(self, action, name):
+    def __init__(self, action):
         self.actionId = action["id"]
         self.actionType = "warden"
         self.file = action["warden"]["configfile"]
-        self.name = name
 
         try:
-            config = warden_client.read(self.file)
-            config["name"] = self.name
-            self.client = warden_client.Client(**config)
+            self.client = warden_client.Client(**warden_client.read_cfg(self.file))
         except ValueError as e:
             logger.error("Failed to load Warden config file '%s'\n%s\n", self.file, e)
             exit(1)
 
     def run(self, record):
-        self.client.sendEvents([record])
+        self.client.sendEvents([json.dumps(record)])
 
     def __del__(self):
         self.client.close()
