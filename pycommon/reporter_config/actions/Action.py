3b7b69105fd8fff2cd59399356f9d869705ed38e
Tomas Cejka <cejkat@cesnet.cz>
2017-08-09 23:27:41 +0200

    pycommon: set logger in the base class Action

--- pycommon/reporter_config/actions/Action.py
+++ pycommon/reporter_config/actions/Action.py
@@ -1,6 +1,9 @@
-class Action():
-    actionId = None
-    actionType = None
+import logging as log
+
+class Action(object):
+    def __init__(self, actionId = None, actionType = None):
+        super(Action, self).__init__()
+        self.logger = log.getLogger(__name__)
 
     def run(self, record):
         raise Exception("Run method not implemented")
