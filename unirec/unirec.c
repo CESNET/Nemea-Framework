7ca9538039e3b0a1c589671c775b2e54f4030f0f
Tomas Cejka <cejkat@cesnet.cz>
2019-03-19 12:26:31 +0100

    unirec: time: add support for UTF timestamp string (ending with 'Z')
    The function did not allow a timestamp format that is usually used in
    IDEA.  This patch allows such string to be converted into UniRec
    timestamp, e.g., "2018-06-27T16:52:54Z".

--- unirec/unirec.c
+++ unirec/unirec.c
@@ -1349,7 +1349,7 @@ uint8_t ur_time_from_string(ur_time_t *ur, const char *str)
 
    res = strptime(str, "%FT%T", &t);
    /* parsed to sec - msec delimiter */
-   if ((res != NULL) && ((*res == '.') || (*res == 0))) {
+   if ((res != NULL) && ((*res == '.') || (*res == 0) || (*res == 'z') || (*res == 'Z'))) {
       sec = timegm(&t);
       if (sec != -1) {
          if (*res != 0 && *++res != 0) {
@@ -1357,11 +1357,14 @@ uint8_t ur_time_from_string(ur_time_t *ur, const char *str)
             memset(frac_buffer, '0', 9);
             frac_buffer[9] = 0;
 
-            // now "res" points to the beginning of the fractional part,
-            // which have at leat one char.
+            // now "res" points to the beginning of the fractional part or 'Z' for UTC timezone,
+            // which have at least one char.
             // Expand the number by zeros to the right to get it in ns
             // (if there are more than 9 digits, truncate the rest)
             size_t frac_len = strlen(res);
+            if (frac_len > 0 && (res[frac_len - 1] == 'z' || res[frac_len - 1] == 'Z')) {
+                frac_len--;
+            }
             if (frac_len > 9) {
                 frac_len = 9;
             }
