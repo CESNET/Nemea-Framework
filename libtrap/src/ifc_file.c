7a4b4b0def540a30189deb3624cda471d977b11d
SiskaPavel <siskapa1@fit.cvut.cz>
2021-03-05 21:39:20 +0100

    libtrap: Fixed flush to file.

--- libtrap/src/ifc_file.c
+++ libtrap/src/ifc_file.c
@@ -652,11 +652,21 @@ static inline void insert_into_buffer(file_buffer_t *buffer, const void *data, u
    buffer->wr_index += (size + sizeof(size));
 }
 
+/**
+ * \brief Flush buffer to file.
+ *
+ * \param[in] priv      pointer to module private data
+ *
+ */
 void file_flush(void *priv)
 {
    int result;
    file_private_t *c = (file_private_t *) priv;
    file_buffer_t *buffer = &c->buffer;
+
+   /* Do not flush empty buffer. */
+   if (buffer->wr_index == 0)
+      return;
 
    finish_buffer(buffer);
 
@@ -664,6 +674,9 @@ void file_flush(void *priv)
 
    if (result == TRAP_E_OK) {
       __sync_add_and_fetch(&c->ctx->counter_send_buffer[c->ifc_idx], 1);
+
+      /* Flush buffer to file. */
+      fflush(c->fd);
 
       /* Reset buffer and insert the message if it was not inserted. */
       buffer->wr_index = 0;
