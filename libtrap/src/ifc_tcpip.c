bb26c4849f184ffb72944769eb8d202ca451956a
Tomas Cejka <cejkat@cesnet.cz>
2023-08-28 21:34:23 +0200

    libtrap: check return value of t_rb_at() to get next container
    If t_rb_at() returns NULL, we cannot access its content (such as the
    buffer or ref_counter).  There is currently continue after this test to
    go back to the start of the loop and check is_terminated.

--- libtrap/src/ifc_tcpip.c
+++ libtrap/src/ifc_tcpip.c
@@ -1126,6 +1126,10 @@ send_blocking_mode(void *arg)
 
       // get next container
       t_cont = t_rb_at(&c->t_mbuf.to_send, cl->container_id);
+      if (t_cont == NULL) {
+         // we cannot operate with NULL t_cont
+         continue;
+      }
           
       buffer = t_cont->buffer;
       pending_bytes = t_cont->used_bytes;
@@ -1192,6 +1196,10 @@ again_set_container:
           
       // get next container
       t_cont = t_rb_at(&c->t_mbuf.to_send, cl->container_id);
+      if (t_cont == NULL) {
+         // we cannot operate with NULL t_cont
+         continue;
+      }
 
       // container is no longer available
       if (t_cont_acquiere(t_cont) < 1 || __sync_fetch_and_add(&t_cont->idx, 0) != cl->container_id) {
